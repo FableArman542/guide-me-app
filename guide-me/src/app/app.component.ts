d73776c7331292d25e0690392fae93b5972867c2
Arman <armanfgtx@gmail.com>
2022-07-11 19:01:06 +0100

    added remember login

--- guide-me/src/app/app.component.ts
+++ guide-me/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private router: Router,
+    public afAuth: AngularFireAuth,
+    public platform: Platform) {}
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.afAuth.user.subscribe(
+        (user) => {
+          if (user) {
+            this.router.navigate(['/tabs/tab1']);
+          } else {
+            this.router.navigate(['/']);
+          }
+        }
+      );
+    });
+  }
 }
